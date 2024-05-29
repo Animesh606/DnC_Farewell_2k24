479719e38b42cc5927a82090f89a51f5b89271e5
Animesh <2025.cse18.animeshbag@gmail.com>
2024-05-29 23:50:45 +0530

    Upgraded slide

---
+++ script.js
@@ -0,0 +1,66 @@
+const names = document.querySelectorAll('.name');
+const directions = ['top', 'right', 'bottom', 'left'];
+let currentIndex = 0;
+
+function showName() {
+    const nameElement = names[currentIndex];
+    const direction = directions[Math.floor(Math.random() * directions.length)];
+    const startPosition = getStartPosition(direction);
+    const centerPosition = getCenterPosition();
+
+    nameElement.style.transition = 'none';
+    nameElement.style.top = startPosition.top;
+    nameElement.style.left = startPosition.left;
+    nameElement.style.opacity = 1;
+
+    setTimeout(() => {
+        nameElement.style.transition = 'all 1s ease';
+        nameElement.style.top = centerPosition.top;
+        nameElement.style.left = centerPosition.left;
+    }, 10);
+
+    setTimeout(() => {
+        nameElement.style.transition = 'all 1s ease';
+        nameElement.style.top = startPosition.top;
+        nameElement.style.left = startPosition.left;
+        nameElement.style.opacity = 0;
+    }, 3000);
+
+    currentIndex = (currentIndex + 1) % names.length;
+    setTimeout(showName, 4000);
+}
+
+function getStartPosition(direction) {
+    const startPosition = {
+        top: '-36%',
+        left: '40%'
+    };
+    switch (direction) {
+        case 'top':
+            startPosition.top = '-10%';
+            startPosition.left = '50%';
+            break;
+        case 'right':
+            startPosition.top = '50%';
+            startPosition.left = '110%';
+            break;
+        case 'bottom':
+            startPosition.top = '110%';
+            startPosition.left = '50%';
+            break;
+        case 'left':
+            startPosition.top = '50%';
+            startPosition.left = '-10%';
+            break;
+    }
+    return startPosition;
+}
+
+function getCenterPosition() {
+    return {
+        top: '-36%',
+        left: '40%'
+    };
+}
+
+showName();
